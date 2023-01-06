362cd42856f29047fe5e9d8fd52afe453f1cdfb5
datastraine <anthony.straine@gmail.com>
2023-01-06 16:10:31 -0600

    Add initial script to github

---
+++ GetVidYT.py
@@ -0,0 +1,100 @@
+import os
+import googleapiclient.discovery
+import pandas as pd
+import numpy as np
+import json
+import re
+from FediBotEnv import Goog # <-- your dev key location
+from datetime import datetime, timedelta
+
+def get_channel_video_info_csv(channelId):
+    '''
+    Using the YouTube API v3 and youtube_transcript_api get the following into a single DF
+    * A channel's upload ID and each video's ID and Title 'Video ID', 'Description', 'Published', 'Duration', Number of Comments, 
+      and whether the video in question was a live stream or not for each video using the snippet, contentDetails, liveStreamingDetails, and statistics from the YouTube API v3
+    '''
+
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    # building blocks for the api call 
+    api_service_name = "youtube"
+    api_version = "v3"
+    DEVELOPER_KEY = Goog
+
+    # create the call object using google discovery
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey = DEVELOPER_KEY)
+   
+    # execute the reponse and get the kinds of data we need (content details)
+    response = youtube.channels().list(
+        id=channelId,
+        part="contentDetails"
+    ).execute()
+
+    # from the executed response, save the uploads playlist id. This will be used to get the videos. 
+    # This is not always the same as the channel Id
+    uploadId = response['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+    
+    
+    # create an empty video list to store the video dict obs
+    videos = []
+    # create a next page token object and give it a none value to be used in the first itteration
+    npt = None
+    
+    # use a while loop to continiously run the script it contains until it gets through all pages to grab all video links
+    while 1:
+        
+        # execute the playlistitems reponse using the paramaters provided
+        response = youtube.playlistItems().list(playlistId = uploadId,
+                                                part="snippet",
+                                                maxResults=50,
+                                                pageToken = npt
+                                               ).execute()
+        
+        # append the reponse items to the videos list
+        videos += response['items']
+        
+        # reassign the next page token to what is found
+        npt = response.get('nextPageToken')
+        
+        # if npt is none break out of the while loop
+        if npt is None:
+            break   
+    
+    # create a vid_df from the normalized videos list of dicts that we created above
+    vid_df = pd.json_normalize(videos) 
+    
+    # reduce the vid_df obj to only the vid and channel information and the rename the columns
+    vid_df = vid_df[['snippet.videoOwnerChannelId', 'snippet.channelTitle', 'snippet.title', 'snippet.resourceId.videoId', 'snippet.playlistId']]
+    vid_df.columns = ['Channel ID', 'Channel Title', 'Video Title', 'Video ID', 'Playlist ID']
+   
+    # create an empty df 
+    vid_deets = pd.DataFrame()    
+    
+    # create a for loop for on the list of Video IDs from our vid_df
+    for vid in vid_df['Video ID']:
+        
+        # then for each vid, execute the videos().list() to get the vid information
+        response = youtube.videos().list(
+            part="snippet,contentDetails,statistics, liveStreamingDetails",
+            id=vid).execute()
+
+        # normalize the response of each vid
+        df = pd.json_normalize(response['items']) 
+             
+        # append the resulting df
+        vid_deets = vid_deets.append(df)
+        
+    # once the vid_deets df is fully created created, pair down the df to the columns we only care about, rename the columns, and reset the index
+    vid_deets = vid_deets[['id', 'snippet.description', 'snippet.publishedAt', 'contentDetails.duration','statistics.viewCount',
+         'statistics.likeCount', 'snippet.tags', 'statistics.commentCount', 'liveStreamingDetails.actualStartTime']]
+    
+    vid_deets.columns = ['Video ID', 'Description', 'Published', 'Duration',
+           'Views', 'Likes', 'Tags', 'Comment Count', 'Live']
+    vid_deets.reset_index(drop=True, inplace=True)
+                    
+    # merge the vid_deets and vid_df together and export the CSV                    
+    vid_df = vid_df.merge(vid_deets, on='Video ID')
+    vid_df.to_csv(re.sub(r"^\s+", "", vid_df['Channel Title'][0]) + "_VideInfo.csv", index=False)
---
+++ TootVid.py
@@ -0,0 +1,122 @@
+import os
+import pandas as pd
+import isodate
+import random
+from datetime import datetime, date, timedelta
+from mastodon import Mastodon
+from FediBotEnv import Masto, Goog # <-- your enviorment creds
+from GetVidYT import get_channel_video_info_csv # <--- the script to build the YouTube Video Details data frame
+from time import sleep
+
+# create a location directory to save the dontsave file to by using os expanduser and assign location based on folder path
+home = os.path.expanduser('~')
+location = os.path.join(home, 'Documents/Scripts') # <-- replace with what ever you want
+
+# set up mastodon.py API connection
+m = Mastodon(access_token=Masto, api_base_url="https://federated.press/") # <-- replace with your mastodon instance url
+
+# assign path obj to the direct path of the channel's info CSV
+path = location + "VideInfo.csv" # <-- your video info URL
+
+# assign a date to a rerunyt obj. This is used to determine if we need to pull the channel info again. I have it set to get the file every 3 days
+rerunyt = datetime.fromtimestamp(os.path.getmtime(path)) + timedelta(days=3)
+
+# use a simple if check to determine if the channel info needs to be rerun
+if rerunyt < datetime.today():
+    print("Updating CSV")
+    # run the get_channel_video_info_csv scrip
+    get_channel_video_info_csv('UCVQurupoN0sXYIUBZyneZQA')
+    print("Done getting channel info")
+    
+else: print("No need to update CSV")
+
+# read the channel info csv into a df
+df = pd.read_csv(path)
+# turn published column into a datetime object and localize time zone for easier use 
+df["Published"] =  pd.to_datetime(df['Published']).dt.tz_localize(None)
+# use apply and lambda with the isodate function to turn duration into seconds
+df['Duration'] = df['Duration'].apply(lambda x: isodate.parse_duration(x).total_seconds())
+# finally eliminate any video that is older than 28 days (lenght of cycle), remove live videos, and any video that is a short
+df = df[(df['Published'] >= pd.Timestamp('today') - pd.offsets.Day(28)) & (df["Live"].isna()) & (df['Duration'] >= 60)].reset_index()
+
+# assign all videos to a video id list and create an empty dontpost list
+vidlist = list(df['Video ID'])
+dontpost = []
+
+# using try, try to open and read dontpost file and if it fails create a dontpost file with placeholder inside
+try:
+    with open(f"{location}\dontpost.txt", 'r') as fobj:
+        # remove linebreak from a current name
+        # linebreak is the last character of each line
+        x = fobj[:-1]
+        print("Dontpost file exist!")
+        # add current item to the list
+
+except: 
+    print("Creating dontpost list")
+    with open(f"{location}\dontpost.txt",  'a+') as fobj:
+        if os.stat(f"{location}\dontpost.txt").st_size == 0:
+            fobj.write('Placeholder')
+
+# read the dontpost file into the dontpost list
+with open(f"{location}\dontpost.txt", 'r') as fp:
+    for line in fp:
+        # remove linebreak from a current name
+        # linebreak is the last character of each line
+        x = line[:-1]
+
+        # add current item to the list
+        dontpost.append(x)
+
+# compare the vidlist to the dontpost list and remove any video id that is in the dontpost list        
+postit = [x for x in vidlist if x not in dontpost]
+
+# create a post obj to store the result of the random choice in the future.
+post = 'rando'
+
+# check to ensure if the postit list has items in it. If it does, randomly select a video id from it. If it doesn't, randomly select a video id from the df
+if len(postit) > 0:
+    post = random.choice(postit) 
+else:
+    post = random.chocice(list(df['Video ID']))
+
+# append the video ID to the dontpost list    
+dontpost.append(post)
+
+# use random choice to randomly pick a video from the postit list and then append that video to the dontpost list
+        
+# toot the description and the video link of the randomly choosen post video
+# Note: The whole toot has to be under 500 characters for most mastodon instances. 
+# If you aren't sure yours will be, replace the first part with whatever you want to say in your script
+m.toot(f"{list(df[df['Video ID']==post]['Description'])[0]} {'https://www.youtube.com/watch?v=' + list(df[df['Video ID']==post]['Video ID'])[0]}")
+
+print("Toot complete!")
+
+# create a rerundontpost obj to store the date we want to delete the dontpost file to start over. I have it set to 14 days
+rerundontpost = datetime.fromtimestamp(os.path.getmtime(f"{location}\dontpost.txt")) + timedelta(days=14)
+
+# use an if else statement to check to see if dontpost video is old enough to replace 
+if rerundontpost < datetime.today():
+    # if it is delete the file and then append the last video posted to it
+    print("Replacing dontrun")
+    # run the get_channel_video_info_csv scrip
+    os.remove(f"{location}\dontpost.txt") 
+    with open(f"{location}\dontpost.txt", 'a+') as fobj:
+        if not str(fobj).endswith('\n'):
+            fobj.write('\n')
+            fobj.write(post)
+        else:
+            fobj.write(post)
+
+# else, simply append the post video 
+else:
+    print("Updating dontrun")
+    with open(f"{location}\dontpost.txt", 'a+') as fobj:
+        if not str(fobj).endswith('\n'):
+            fobj.write('\n')
+            fobj.write(post)
+        else:
+            fobj.write(post)
+
+print("TootBot run complete")
+sleep(2)
